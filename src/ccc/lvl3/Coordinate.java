5e1355f638b9571918e9fcf952b1957d7cfae1b8
m1ckey <ch.michael.krickl@gmail.com>
2019-03-22 10:49:48 +0100

    cord

---
+++ src/ccc/lvl3/Coordinate.java
@@ -0,0 +1,15 @@
+package ccc.lvl3;
+
+public class Coordinate
+{
+
+  public final int i, j;
+  public final Color c;
+
+  public Coordinate(int i, int j, Color c)
+  {
+    this.i = i;
+    this.j = j;
+    this.c = c;
+  }
+}
