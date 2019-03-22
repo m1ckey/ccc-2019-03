16d8232e37c61489722f7606be710abf8027932e
m1ckey <ch.michael.krickl@gmail.com>
2019-03-22 10:04:46 +0100

    color

---
+++ src/ccc/lvl1/Color.java
@@ -0,0 +1,13 @@
+package ccc.lvl1;
+
+public class Color
+{
+  public final int red, green, blue;
+
+  public Color(int red, int green, int blue)
+  {
+    this.red = red;
+    this.green = green;
+    this.blue = blue;
+  }
+}
